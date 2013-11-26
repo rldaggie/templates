e5cd8e83cbd9980c0e74f1180110bf3d3085d90c
Riley Dallas <rldaggie@gmail.com>
2013-11-26 15:33:35 -0600

    Added devise conditional

--- app_template.rb
+++ app_template.rb
@@ -1,2 +1,11 @@
 generate :controller, "welcome index"
 route "root to: 'welcome\#index'"
+
+if yes? "Devise?"
+  gem 'devise'
+  run 'bundle install'
+  generate 'devise:install'
+  generate 'devise:views'
+  generate 'devise user'
+  run 'rake db:migrate'
+end
