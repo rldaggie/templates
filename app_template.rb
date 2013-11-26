ca8f7b298b6c0256b375a1d0bdf388e30071702c
Riley Dallas <rldaggie@gmail.com>
2013-11-26 16:39:01 -0600

    Added application erb

--- app_template.rb
+++ app_template.rb
@@ -1,6 +1,13 @@
-generate 'controller', "welcome index"
-route "root to: 'welcome\#index'"
+# Twitter Bootstrap
+remove_file 'app/views/layouts/application.html.erb'
+get 'https://raw.github.com/rldaggie/templates/master/application.html.erb', 'app/views/layouts/application.html.erb'
 
+
+# Root controller
+generate 'controller', "welcome index"
+route "root 'welcome\#index'"
+
+# Devise
 if yes? "Devise?"
   gem 'devise'
   run 'bundle install'
