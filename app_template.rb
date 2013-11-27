93e3013f8ea04c4d2a7b908bc974b6d1fc127b0d
Riley Dallas <rldaggie@gmail.com>
2013-11-26 18:14:07 -0600

    Changed devise views

--- app_template.rb
+++ app_template.rb
@@ -1,8 +1,10 @@
+the_url = "https://raw.github.com/rldaggie/templates/master/"
+
 # Twitter Bootstrap
 remove_file 'app/views/layouts/application.html.erb'
-get 'https://raw.github.com/rldaggie/templates/master/application.html.erb', 'app/views/layouts/application.html.erb'
+get "#{the_url}app/views/layouts/application.html.erb", 'app/views/layouts/application.html.erb'
 remove_file 'app/helpers/application_helper.rb'
-get 'https://raw.github.com/rldaggie/templates/master/application_helper.rb', 'app/helpers/application_helper.rb'
+get "#{the_url}app/helpers/application_helper.rb", 'app/helpers/application_helper.rb'
 
 # Set app name in en.yml
 gsub_file 'config/locales/en.yml', '  hello: "Hello world"', "  app_name: '#{@app_name}'"
@@ -19,9 +21,16 @@ generate 'devise:views'
 generate 'devise user'
 run 'rake db:migrate'
 
+# Change Devise Views
+['passwords/new', 'passwords/edit', 'registrations/new', 'registrations/edit', 'sessions/new', 'shared/_links'].each do |the_devise|
+  the_path = "app/views/devise/#{the_action}.html.erb"
+  get "#{the_url}#{the_path}", the_path
+end
+
+
 # Add scaffold html
 ['_form', 'edit', 'index', 'new', 'show'].each do |the_action|
   the_path = "lib/templates/erb/scaffold/#{the_action}.html.erb"
-  get "https://raw.github.com/rldaggie/templates/master/#{the_path}", the_path
+  get "#{the_url}#{the_path}", the_path
 end
-get "https://raw.github.com/rldaggie/templates/master/app/views/shared/_errors.html.erb", 'app/views/shared/_errors.html.erb'
+get "#{the_url}app/views/shared/_errors.html.erb", 'app/views/shared/_errors.html.erb'
